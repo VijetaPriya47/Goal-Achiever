7ab0201706a7f6f2f0edd83305456764fafd8812
Vijeta Priya <67923889+RonnieAdler@users.noreply.github.com>
2024-06-15 06:34:36 +0000

    Add files via upload

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -10,7 +10,18 @@ contract Todolist {
         bool completed;
     }
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+      );
+
     mapping (uint => Task) public tasks;
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+      );
 
     constructor() public {
         createTask("Complete DSA");
@@ -19,9 +30,16 @@ contract Todolist {
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount]= Task (taskCount, _content,false) ;
-
+        emit TaskCreated(taskCount, _content, false);
     }
 
+    function toggleCompleted(uint _id) public {
+
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+      }
 
 
 }
